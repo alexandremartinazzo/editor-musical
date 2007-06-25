ee6670423789b6923c956aaccd309e40e546e9ae
brunogola <brunogola@bd8bdd78-b72f-0410-8a1f-3b4cb2e4b520>
2007-06-25 21:43:04 +0000

    metodos play() e stop() funcionando no csound.py xokb.py usa esses metodos

--- sound/XOKeyBoard/xokb.py
+++ sound/XOKeyBoard/xokb.py
@@ -24,6 +24,7 @@ n=3000)
 class XOKeyBoard:
 	""" interface para testar a classe CsndPlayer """
 	oct=4
+	playing = []
 	def key_press(self,widget,event,data=None):
 		if event.string == '[':
 			self.oct-=1
@@ -31,18 +32,20 @@ class XOKeyBoard:
 		elif event.string == ']':
 			self.oct+=1
 			return
-		if event.string in notes.keys():
-			self.csound.playInstr(ORGAN,notes[event.string],0.5,self.oct)
+		if event.string in notes.keys() and event.string not in self.playing:
+			self.playing.append(event.string)
+			self.csound.play(ORGAN,notes[event.string],self.oct)
 	def key_release(self,widget,event,data=None):
-		if event.string in ['a','b','c','d','e','f','g']:
-			self.csound.pause()
+		if event.string in notes.keys() and event.string in self.playing:
+			self.playing.pop(self.playing.index(event.string))
+			self.csound.stop()
 	def destroy(self, widget, data=None):
  		gtk.main_quit()
 	def __init__(self):
-		self.csound = CsndPlayer()
+		self.csound = CsndPlayer('instruments.csd')
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.window.connect("key_press_event",self.key_press) 
-		#self.window.connect("key_release_event",self.key_release) 
+		self.window.connect("key_release_event",self.key_release) 
 		self.window.connect("destroy", self.destroy)
 		self.window.show()
 	def main(self):
